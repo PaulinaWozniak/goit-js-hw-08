33763bee6ae40f50760b761c17d0709ece3a83e3
Paulina Woźniak <wozniak.paulina08@gmail.com>
2023-03-03 15:45:02 +0100

    homework
    in progress

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(SimpleLightbox);
+
+const ulGallery = document.querySelector('.gallery');
+
+
+const markup = galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+    <img 
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+    </a>`
+}). join('');
+
+ulGallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
