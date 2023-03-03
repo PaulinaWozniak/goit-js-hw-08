23da547780c51746c045cd1d609970f99c98bb62
Paulina Woźniak <wozniak.paulina08@gmail.com>
2023-03-03 17:29:47 +0100

    Homework
    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("#vimeo-player")
+const player = new Vimeo.Player(iframe);
+const lSKey = "videoplayer-current-time";
+
+
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem(lSKey, data.seconds);
+}, 1000));
+
+
+function resumeTime() {
+    let setTime = localStorage.getItem(lSKey);
+    if (setTime) player.setCurrentTime(setTime).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'Error' :
+                break;
+            default:
+                break;
+        }
+    });
+
+    console.log(setTime);
+}
+resumeTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textArea');
+let feedbackFormState = {};
+
+
+inputData();
+
+const formInput = (event) => {
+    feedbackFormState[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState))
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const elements = {email: email.value, message: message.value}
+    console.log(elements);
+    email.value = "";
+    message.value = "";
+    feedbackFormState = {};
+    localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', throttle((formInput), 500));
+form.addEventListener('submit', handleSubmit);
+
+function inputData() {
+    let data = localStorage.getItem('feedback-form-state');
+    console.log(data);
+    if (data) {
+        data = JSON.parse(data);
+        Object.entries(data).forEach(([name, value]) => {
+            feedbackFormState[name] = value;
+            form.elements[name].value = value;
+        })
+    } else {
+        email.value = "",
+        message.value = "";
+    };
+}
